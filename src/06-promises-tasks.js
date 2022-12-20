ed43a55a5a7fd3c98bc493ad6a430ed594c58e39
A-nastasi-a <evsihneyeva@gmail.com>
2022-12-20 16:11:00 +0400

    feat: add solutions for couple of exersices of promises

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -29,18 +29,18 @@
  *                                                    //  Ask her again.';
  */
 function willYouMarryMe(isPositiveAnswer) {
-    const promise = new Promise(((resolve, reject) => {
-        if (typeof isPositiveAnswer === 'boolean') {
-            if (isPositiveAnswer) {
-                resolve('Hooray!!! She said "Yes"!');
-            } else {
-                resolve('Oh no, she said "No".');
-            }
-        } else {
-            reject(new Error('Wrong parameter is passed! Ask her again.'));
-        }
-    }));
-    return promise;
+  const promise = new Promise(((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      if (isPositiveAnswer) {
+        resolve('Hooray!!! She said "Yes"!');
+      } else {
+        resolve('Oh no, she said "No".');
+      }
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  }));
+  return promise;
 }
 
 
@@ -59,8 +59,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises( /* array */ ) {
-    throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -82,8 +82,8 @@ function processAllPromises( /* array */ ) {
  *    })
  *
  */
-function getFastestPromise( /* array */ ) {
-    throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -103,13 +103,13 @@ function getFastestPromise( /* array */ ) {
  *    });
  *
  */
-function chainPromises( /* array, action */ ) {
-    throw new Error('Not implemented');
+function chainPromises(/* array, action */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
-    willYouMarryMe,
-    processAllPromises,
-    getFastestPromise,
-    chainPromises,
+  willYouMarryMe,
+  processAllPromises,
+  getFastestPromise,
+  chainPromises,
 };
